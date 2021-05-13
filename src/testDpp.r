b113c98cc4b966383ff5e97c1778c83cdca77259
nomad <mengling_wu@icloud.com>
2021-05-13 20:26:49 +0800

    v1.0

--- src/testDpp.r
+++ src/testDpp.r
@@ -1,3 +1,3 @@
 install.packages("/Users/menglingwu/Documents/2021/coding/personal_projects/rDppDiversity_0.0.0.9000.tar.gz", repos = NULL, type="source")
 library(rDppDiversity)
-meanC(matrix(c(-0.1, -0.15, 0.2, 0.3, -0.2, -0.3), nrow=3, ncol=2), c(1, 2, 3), 3)
+meanC(matrix(c(-0.1, -0.15, 0.2, 0.3, -0.2, -0.3), nrow=3, ncol=2, byrow=T), c(1, 2, 3), 3)
