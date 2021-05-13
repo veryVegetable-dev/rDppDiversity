012166cc85fd903871ae1151ba5d0998691b1c8d
nomad <mengling_wu@icloud.com>
2021-05-13 15:32:03 +0800

    init

---
+++ R/add.R
@@ -0,0 +1,16 @@
+#' Returns the sum of two numbers
+#'
+#' @param a Number one.
+#' @param b Number two.
+#'
+#' @return Sum of two number.
+#' @export
+#'
+#' @examples
+#' library(rDppDiversity)
+#' add(1, 2)
+add<-function(a,b) {
+  a+b
+}
+
+devtools::document()
